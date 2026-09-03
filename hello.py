ebc9eb734bbea0e58e178b72cdb4af76beba7b11
Jeff <2602465@sit.singaporetech.edu.sg>
2026-09-04 00:08:09 +0800

    Activity 3: Follower Growth tracker

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
+# Activity 1: First Post
 print("==========================")
 print("Welcome here")
 print("My first post!")
 print("==========================")
 
+# Activity 2: Update your Profile and add it to the repository
 username = "cool_creator"
 bio = "Fun Blogger"
 followers = 100
@@ -10,3 +12,13 @@ followers = 100
 print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
+
+# Activity 3: Follwoer Growth tracker
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -= 10
+print("Day 3:", followers)
