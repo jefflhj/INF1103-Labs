a9c1add93d0b70010e242a79e6d5b90b5b0f36f0
Jeff <2602465@sit.singaporetech.edu.sg>
2026-09-04 00:11:51 +0800

    Activity 4: Interactive profile creator

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 # Activity 1: First Post
-print("==========================")
+print("===================")
 print("Welcome here")
 print("My first post!")
-print("==========================")
+print("===================")
 
 # Activity 2: Update your Profile and add it to the repository
 username = "cool_creator"
@@ -22,3 +22,14 @@ print("Day 2:", followers)
 
 followers -= 10
 print("Day 3:", followers)
+
+# Activity 4: Interactive profile creator
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("===================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
