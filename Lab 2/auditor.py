812327573cb7db3652d9b9c7668c2ee2a2110f42
Jeff <2602465@sit.singaporetech.edu.sg>
2026-09-11 01:10:02 +0800

    Requirement 8

--- Lab 2/auditor.py
+++ Lab 2/auditor.py
@@ -1,17 +1,22 @@
 # Requirement 1: Initialize the inventory to zero in the start
 inventory = 0
+rejected_count = 0
 
 # Requirement 2: Run in a continuous loop asking user to enter a stock quantity, until the user types "quit".
 while True:
     stock_input = input("Enter a stock quantity (type 'quit' to exit): ")
 
     if stock_input.lower() == "quit":
+        # Requirement 8: Reporting.
+        print(f"\nTotal Units Processed: {inventory}")
+        print(f"Failed / Rejected Entries: {rejected_count}\n")
         print("Exiting...")
         break
 
     # Requirement 4 & 5: Handle invalid inputs and reject negative numbers.
     if not stock_input.isdigit():
         print("\nPlease enter a valid number.\n")
+        rejected_count += 1
         continue
 
     # Requirement 3: Accept stock values as integers.
