8f1d0e6d7fd87f0c4026ae4eed69fd5e2d762923
Jeff <2602465@sit.singaporetech.edu.sg>
2026-09-25 02:26:43 +0800

    Created new lab 4 requirement function for item names

--- Lab 4/persistent_auditor.py
+++ Lab 4/persistent_auditor.py
@@ -7,12 +7,21 @@
    number of failed entries made by the user excluding delivery cost entries. These pieces of information will be saved and 
    stored in a database text file.
 
+   Lab 4 now allows the user to specify the items in the delivery. Delivery Item ID, Item Name, Quantity.
+   The idea here is that the store orders items from a vendor and records these items for stock taking. This system is used 
+   to keep track of incoming items, not to place an order for new items.
+   This option will not track rejected entries. Possible with a database of valid item names.
+   Note that Delivery = Order and Item = Product.
+
    Stored Variables:
         inventory:              The current total number of items in the store's physical inventory.
         rejected_entries:       The total number of rejected entries made by the user.
         delivery_expenditure:   The running total cost of all deliveries made to the store.
         tax_expenditure:        The running total tax cost of all deliveries made to the store.
-        delivery_history:       A list of lists for every delivery made to the store and their details.
+        delivery_history:       A list of lists for every delivery made to the store and their details. #### not done yet
+        delivery_item_id:       Unique ID for the item in the current delivery.
+        item_name:              Item's name.
+        item_quantity:          How many of that item.
 """
 
 import os
@@ -81,18 +90,20 @@ def get_delivery_cost():
             continue
 
 
-def update_local_database(database, delivery_item_count, rejected_entries_count, delivery_cost, delivery_tax):
-    database["inventory"] = database.get("inventory", 0) + delivery_item_count
-    database["rejected_entries"] = database.get("rejected_entries", 0) + rejected_entries_count
-    database["delivery_expenditure"] = database.get("delivery_expenditure", 0) + delivery_cost
-    database["tax_expenditure"] = database.get("tax_expenditure", 0) + delivery_tax
-    database["delivery_history"] = []
-
-
 def calculate_delivery_tax(delivery_cost):
     tax_rate = 0.1
 
     return round(delivery_cost * tax_rate, 2)
+
+
+def update_local_database(database, delivery_item_count, rejected_entries_count, delivery_cost, delivery_tax, delivery_record):
+    database["inventory"] = database.get("inventory", 0) + delivery_item_count
+    database["rejected_entries"] = database.get("rejected_entries", 0) + rejected_entries_count
+    database["delivery_expenditure"] = database.get("delivery_expenditure", 0) + delivery_cost
+    database["tax_expenditure"] = database.get("tax_expenditure", 0) + delivery_tax
+    if delivery_record is None:
+        return
+    database["delivery_history"].append(delivery_record)
 
 
 def print_summary_report(database):
@@ -109,9 +120,77 @@ def check_overstock(database, delivery_item_count):
     return True
 
 
+def print_option_menu():
+    print("========= Choose An Option =========")
+    print("1) Enter New Delivery Quantity Only")
+    print("2) Enter New Delivery With Item Names")
+    print("3) Save & Quit")
+
+    while True:
+        chosen_option = input(">>> ")
+
+        if chosen_option != "1" or chosen_option != "2" or chosen_option != "3":
+            print("Please select either '1', '2' or '3'.")
+            continue
+        else:
+            return chosen_option
+
+
+def enter_new_delivery(database):
+    item_number = 0
+    delivery_record = []
+    total_quantity = 0
+
+    while True:
+        item_number += 1
+        delivery_item_id = (len(database["delivery_history"])*1000)+(item_number)
+        item_name = input("Enter Product Name: ")
+        item_quantity = input("Enter Quantity: ")
+
+        print("\nNew Delivery Item Recorded:")
+        print(f"{delivery_item_id},{item_name},{item_quantity}")
+
+        delivery_record.append((delivery_item_id, item_name, item_quantity))
+        total_quantity += item_quantity
+
+        if input("Add another item? ('yes' / 'no')").lower() == "no":
+            delivery_cost = get_delivery_cost()
+            delivery_tax = calculate_delivery_tax(delivery_cost)
+            update_local_database(database, total_quantity, 0, delivery_cost, delivery_tax, delivery_record)
+            return
+
+
+def main2():
+    database_filepath = os.getenv("DATABASE_FILE", "/auditor/database/database.txt")
+    database = get_database(database_filepath)
+    
+    while True:
+        chosen_option = print_option_menu()
+
+        if chosen_option == "3":
+            save_to_database(database_filepath, database)
+            print_summary_report(database)
+            return
+        
+        elif chosen_option == "2":
+            enter_new_delivery(database)
+
+        elif chosen_option == "1":
+            get_delivery_item_count()
+
+
+
+
+
+
+
+
+
+
+
+
 def main():
     database_filepath = os.getenv("DATABASE_FILE", "/auditor/database/database.txt")
-
     database = get_database(database_filepath)
 
     while True:
@@ -127,7 +206,7 @@ def main():
         delivery_tax = calculate_delivery_tax(delivery_cost)
 
         if check_overstock(database, delivery_item_count):
-            update_local_database(database, delivery_item_count, rejected_entries_count, delivery_cost, delivery_tax)
+            update_local_database(database, delivery_item_count, rejected_entries_count, delivery_cost, delivery_tax, None)
 
 
 if __name__ == "__main__":
